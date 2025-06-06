af24a905f616d18615a49df8a2f1e5e680605bfd
blandh <blandh26@gmail.com>
2025-06-06 14:16:10 +0800

    처음

---
+++ ZR.Model/Social/Dto/SocialFansDto.cs
@@ -0,0 +1,27 @@
+﻿using ZR.Model.Content.Dto;
+
+namespace ZR.Model.social
+{
+    /// <summary>
+    /// 粉丝表
+    /// </summary>
+    public class SocialFansDto
+    {
+        [JsonIgnore]
+        public long PId { get; set; }
+        public long Userid { get; set; }
+        public long ToUserid { get; set; }
+        [JsonIgnore]
+        public DateTime FollowTime { get; set; }
+        [JsonIgnore]
+        public string UserIP { get; set; }
+        public int Status { get; set; }
+
+        public ArticleUser User { get; set; }
+    }
+
+    public class FansQueryDto : PagerInfo
+    {
+        public int SelectType { get; set; }
+    }
+}
---
+++ ZR.Model/Social/SocialFans.cs
@@ -0,0 +1,20 @@
+﻿namespace ZR.Model.social
+{
+    /// <summary>
+    /// 粉丝表
+    /// </summary>
+    [SugarTable("social_fans")]
+    public class SocialFans
+    {
+        [SugarColumn(IsPrimaryKey = true)]
+        public long PId { get; set; }
+        public long Userid { get; set; }
+        public long ToUserid { get; set; }
+        /// <summary>
+        /// 备注名
+        /// </summary>
+        public string RemarkName { get; set; }
+        public DateTime FollowTime { get; set; }
+        public string UserIP { get; set; }
+    }
+}
